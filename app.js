448c7dfd740f3fbf4cee614b209553c8776ec521
Shane <shanewilsonpro@gmail.com>
2021-02-17 20:57:34 -0800

    path inside breadcrumb and loop through folder elements

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ const http = require('http');
 const respond = require('./lib/respond.js');
 
 //connection settings
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 //create server
 const server = http.createServer(respond);
 
 //listen to client requests on the specific port, the port should be available
 server.listen(port, () => {
-    console.log('listening on port: ${port}');
+    console.log('listening on port: ' + port + '');
 });
 
