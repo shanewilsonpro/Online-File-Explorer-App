4cab42053dd117d58d3d8714b18501ddf1084e63
Shane <shanewilsonpro@gmail.com>
2021-02-18 20:28:34 -0800

    fixes bugs and clean code

--- playground/child_process.js
+++
@@ -1,9 +0,0 @@
-const {execSync} = require('child_process');
-
-try {
-    const result = execSync('du -sh "/Users/shanewilson/Documents/Developments/Web_Dev/Online-File-Explorer-App"').toString();
-    console.log(result);
-}catch(err) {
-    console.log('Error: ' + err);
-}
-
--- playground/path.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-
-console.log(path.join('Node', 'Project1'));
-
-console.log(path.normalize('../../Node/../Built in Modules fs'));
-
-console.log(path.resolve('../../Node/../Built in Modules fs'));
